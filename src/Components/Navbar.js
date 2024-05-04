5521f5dca4011e17b0952782e5e6f940a3729df4
eswar <eswar@gmail.com>
2024-05-04 11:14:15 +0530

    Your commit message here

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,7 @@ export default function Navbar() {
       <div class="navbar-nav">
         <a class="nav-link active" aria-current="page" href="Home">Home</a>
         <a class="nav-link" href="About Us">About Us</a>
-        <a class="nav-link" href="Sign up">Sign up</a>
+        <a class="nav-link" href="Signup">Sign up</a>
         <a class="nav-link" href="Login">Login </a>
         <a class="nav-link" href="Contact Details">Contact Details</a>
         
